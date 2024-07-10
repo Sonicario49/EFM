f4ad2b3635fdab907f948d72309cd798c74f52e3
Sonicario49 <noureddine.bendanoune@gmail.com>
2024-07-10 18:17:53 +0200

    Pcommit

---
+++ src/components/contact.jsx
@@ -0,0 +1,146 @@
+import { useState } from "react";
+import emailjs from "emailjs-com";
+import React from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  message: "",
+};
+export const Contact = (props) => {
+  const [{ name, email, message }, setState] = useState(initialState);
+  const [status, setStatus] = useState("");
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setState((prevState) => ({ ...prevState, [name]: value }));
+  };
+  const clearState = () => setState({ ...initialState });
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Construct the email parameters
+    const templateParams = {
+      from_name: name,
+      from_email: email,
+      message: message,
+    };
+
+    emailjs
+      .send("service_8d8akcd", "template_q7wdzzv", templateParams, "cYzT0a5e8_o3JmOn2")
+      .then(
+        (result) => {
+          console.log(result.text);
+          clearState();
+          setStatus("Email bien envoyé !"); // Update status on success
+        },
+        (error) => {
+          console.log(error.text);
+          setStatus("Erreur lors de l'envoi de l'email."); // Update status on error
+        }
+      );
+  };
+  return (
+    <div>
+      <div id="contact">
+        <div className="container">
+          <div className="col-md-8">
+            <div className="row">
+              <div className="section-title">
+                <h2>Contactez-nous</h2>
+                <p>
+                  Veuillez remplir le formulaire ci-dessous pour nous envoyer un email et nous vous répondrons dès que possible.
+                </p>
+              </div>
+              <form name="sentMessage" validate onSubmit={handleSubmit}>
+                <div className="row">
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="form-control"
+                        placeholder="Nom"
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="form-group">
+                      <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="form-control"
+                        placeholder="Email"
+                        required
+                        onChange={handleChange}
+                      />
+                      <p className="help-block text-danger"></p>
+                    </div>
+                  </div>
+                </div>
+                <div className="form-group">
+                  <textarea
+                    name="message"
+                    id="message"
+                    className="form-control"
+                    rows="4"
+                    placeholder="Message"
+                    required
+                    onChange={handleChange}
+                  ></textarea>
+                  <p className="help-block text-danger"></p>
+                </div>
+                <div id="success"></div>
+                <button type="submit" className="btn btn-custom btn-lg">
+                  Envoyer
+                </button>
+              </form>
+              {status && <p className="status-message">{status}</p>} {/* Display status message */}
+            </div>
+          </div>
+          <div className="col-md-3 col-md-offset-1 contact-info">
+            <div className="contact-item">
+              <h3>Contact Info</h3>
+              <p>
+                <span>
+                  <i className="fa fa-map-marker"></i> Adresse
+                </span>
+                {props.data ? props.data.address : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-phone"></i> Numéro de Contact
+                </span>{" "}
+                {props.data ? props.data.phone : "loading"}
+              </p>
+            </div>
+            <div className="contact-item">
+              <p>
+                <span>
+                  <i className="fa fa-envelope-o"></i> Email
+                </span>{" "}
+                {props.data ? props.data.email : "loading"}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div id="footer">
+        <div className="container text-center">
+          <p>
+            &copy; 2023 Bendanoune Noureddine.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
